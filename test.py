459be714afe8d4baa1c55732994faed70fa6a672
Taaha Bajwa <taaha.s.bajwa@gmail.com>
2023-05-29 21:09:33 +0500

    hooha

---
+++ test.py
@@ -0,0 +1,23 @@
+# Python program to check
+# whether the camera is opened
+# or not
+
+
+import numpy as np
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+while(cap.isOpened()):
+	
+	while True:
+		
+		ret, img = cap.read()
+		cv2.imshow('img', img)
+		if cv2.waitKey(30) & 0xff == ord('q'):
+			break
+			
+	cap.release()
+	cv2.destroyAllWindows()
+else:
+	print("Alert ! Camera disconnected")
